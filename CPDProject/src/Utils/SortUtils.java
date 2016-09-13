14ac56fbcbc5e4920e178c24157a559309e80453
fsalmeida <fabricio.sda@gmail.com>
2016-09-13 09:54:22 -0300

    ShellSort
    Inserção do algoritimo de ordenação shellSort e alteração do caminho do diretório da base de dados para rodar no meu pc

--- CPDProject/src/Utils/SortUtils.java
+++ CPDProject/src/Utils/SortUtils.java
@@ -17,7 +17,7 @@ public class SortUtils {
         sortTimes.put("Insercao Direta", String.valueOf(insertionSort(data)));
 
 //        sortTimes.put("Bubble Sort", String.valueOf(bubbleSort(data)));
-//        sortTimes.put("Shell Sort", String.valueOf(shellSort(data)));
+       sortTimes.put("Shell Sort", String.valueOf(shellSort(data)));
 
         return sortTimes;
 
@@ -45,9 +45,32 @@ public class SortUtils {
         return elapsedTime;
     }
 
-//    private long shellSort(int data[]){
-//
-//    }
+    private long shellSort(SexRatioEntity data[]){
+
+        long startTime = System.currentTimeMillis();
+
+         int h = 1;
+         int j;
+         int n_elementos = data.length;
+         SexRatioEntity c;
+         while(h < n_elementos)
+            h = h * 3 + 1;
+         h = h / 3;
+	     while (h > 0) {
+             for (int i = h; i < n_elementos; i++) {
+                 c = data[i];
+                 j = i;
+                 while (j >= h && data[j - h].getRatio() > c.getRatio()) {
+                    data[j] = data[j - h];
+                    j = j - h;
+                 }
+                 data[j] = c;
+             }
+         h = h / 2;
+    }
+    long elapsedTime = System.currentTimeMillis() - startTime;
+        return elapsedTime;
+}
 
 //    private long bubbleSort(int data[]){
 
